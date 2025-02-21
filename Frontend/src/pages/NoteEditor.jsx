ee4abee8cb17e82e63139e1dd20197270df574fc
Adam Abdelrahman <adam.aaa.abdelrahman@gmail.com>
2025-02-21 10:36:51 -0500

    stuff

--- Frontend/src/pages/NoteEditor.jsx
+++ Frontend/src/pages/NoteEditor.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import MarkdownEditor from "../components/MarkdownEditor";
-import InfiniteCanvas from "../components/Canvas";
+import Canvas from "../components/Canvas";
 import "./NotesEditor.css";
 
 const NotesEditor = () => {
   return (
     <div className="notes-container">
-      <InfiniteCanvas position="left" />
+      <Canvas position="left" />
       <MarkdownEditor />
-      <InfiniteCanvas position="right" />
+      <Canvas position="right" />
     </div>
   );
 };
