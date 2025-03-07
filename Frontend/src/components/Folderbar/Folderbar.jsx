1a8de0687595d5319aa88232cc4fbd18e6ff4475
Ashton <ady@vols.utk.edu>
2025-03-07 18:03:13 -0500

    Implemented Folder system

--- Frontend/src/components/Folderbar/Folderbar.jsx
+++ Frontend/src/components/Folderbar/Folderbar.jsx
@@ -1,7 +1,31 @@
 import styles from './Folderbar.module.scss';
 import { BsChevronBarRight } from "react-icons/bs";
+import { Folders } from '../../FakeFolderData/Data.json'
 
 function Folderbar({setSidebarOpen, SidebarOpen}) {
+  const data = Folders;
+  function getFiles(folder, FolderIndex){
+    return folder.map((file)=>{
+      return (
+        <div className={styles.FileCont} key={file.name} style={{marginLeft: `${FolderIndex*15}px`}}>
+          <p className={styles.FileName}>{file.name}</p>
+        </div>
+      )
+    })
+  }
+  function getFolder(data, FolderIndex){
+    return data.map((folder) =>{
+      console.log(FolderIndex)
+      return(
+        <div className={styles.FolderCont} key={folder.name} style={{marginLeft: `${FolderIndex*15}px`}}>
+          <p className={styles.FolderName}>{folder.name}</p>
+          {folder.folders?getFolder(folder.folders, FolderIndex+1):null}
+          {getFiles(folder.files, FolderIndex+1)}
+        </div>
+      )
+    })
+  }
+
   function handleSidebarChange(){
     setSidebarOpen((prev)=> !SidebarOpen);
   };
@@ -10,7 +34,9 @@ function Folderbar({setSidebarOpen, SidebarOpen}) {
       <div id={styles.ChevronIcon} className={SidebarOpen ? styles.none : ""}>
         <BsChevronBarRight onClick={handleSidebarChange}/>
       </div>
-        <p>Folderbar</p>
+        <div className={styles.FolderSystemCont}>
+          {getFolder(data, 0)}
+        </div>
     </div>
   );
 }
