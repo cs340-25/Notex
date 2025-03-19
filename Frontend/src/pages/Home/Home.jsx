fbf6a84e6d5bfb98e391bd71e25f227b5f3ad429
Perry Shu <perryshuae@gmail.com>
2025-03-19 14:15:33 -0400

    home barely

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.scss';
+import data from '../../FakeFolderData/Data.json';
+import { IoFolder, IoDocumentText, IoFolderOpen } from "react-icons/io5";
+
 
 function Home() {
+  const [folders, setFolders] = useState([]);
+
+  useEffect(() => {
+    if (data.Folders) {
+      setFolders(data.Folders);
+    }
+  }, []);
+
   return (
     <div className="HomeCont">
-      Home
+      <header className="HomeHeader">
+        <h1>notex</h1>
+        <div className="Menu">
+          <button onClick={() => alert('open folder')}><IoFolderOpen />Open Folder</button>
+          <button onClick={() => alert('makes a file')}><IoDocumentText />Make File</button>
+        </div>
+      </header>
+
+      <div className="GridContainer">
+        {folders.map((folder) => (
+          <div key={folder.id} className="GridItem folder">
+            <IoFolder />{folder.name}
+          </div>
+        ))}
+
+        {folders.flatMap(folder => folder.files || []).map((file) => (
+          <div key={file.id} className="GridItem file">
+            <IoDocumentText />{file.name}.{file.type}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
