d9e6e5f5e6ca08d7014ac0be2dc71ab0d513eefb
Perry Shu <perryshuae@gmail.com>
2025-04-28 19:39:01 -0400

    markdown editor done

--- Frontend/src/components/MarkdownEditor/MarkdownEditor.jsx
+++ Frontend/src/components/MarkdownEditor/MarkdownEditor.jsx
@@ -1,21 +1,49 @@
-import React from "react";
-import styles from "./MarkdownEditor.module.scss";
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import styles from './MarkdownEditor.module.scss'; 
 
-const MarkdownEditor = ({ markdown, setMarkdown, textBox }) => {
+export default function MarkdownEditor() {
+  const [title, setTitle] = useState('');
+  const [markdown, setMarkdown] = useState('');
+  const [isEditMode, setIsEditMode] = useState(true);
+
+  const wordCount = markdown.trim() === '' ? 0 : markdown.trim().split(/\s+/).length;
+
   return (
-    <div className={styles.markdownEditorContainer}>
-      <textarea
-        className={styles.markdownEditor}
-        value={markdown}
-        onChange={(e) => setMarkdown(e.target.value)}
-        placeholder="Title"
-      />
-      
+    <div className={styles.markdownEditorWrapper}>
+      <div className={styles.markdownEditor}>
+        {/* title */}
+        <input
+          type="text" placeholder="title" value={title}
+          onChange={(e) => setTitle(e.target.value)} className={styles.markdownTitle}
+        />
+
+        {/* textarea for markdown */}
+        <div className={styles.markdownBody}>
+          {isEditMode && (
+            <textarea value={markdown} onChange={(e) => setMarkdown(e.target.value)}
+              placeholder="body" className={styles.markdownTextarea}
+            />
+          )}
+
+          {!isEditMode && (
+            <div className={styles.markdownPreview}>
+              <ReactMarkdown>{markdown || '*Nothing to preview*'}</ReactMarkdown>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* footer stuff */}
+      <div className={styles.markdownFooter}>
+        <button onClick={() => setIsEditMode(!isEditMode)} className={styles.markdownButton}>
+          {isEditMode && 'Switch to Preview'} {!isEditMode && 'Switch to Edit'}
+        </button>
+
+        <div className={styles.markdownWordcount}>
+          {wordCount} {wordCount === 1 ? 'word' : 'words'}
+        </div>
+      </div>
     </div>
-    
-
-    
   );
-};
-
-export default MarkdownEditor;
+}
