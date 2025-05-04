3e01a045a751f56e3c3b93400fe3a38fc86f8260
Ashton <ady@vols.utk.edu>
2025-05-04 10:51:48 -0400

    Synced folder bar and text editor

--- Frontend/src/pages/NoteEditor/NoteEditor.jsx
+++ Frontend/src/pages/NoteEditor/NoteEditor.jsx
@@ -1,17 +1,38 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, use} from "react";
 import MarkdownEditor from "../../components/MarkdownEditor/MarkdownEditor";
 import Canvas from "../../components/Canvas/Canvas";
 import "./NotesEditor.scss";
+import { useLocation } from "react-router-dom";
 
 const NotesEditor = () => {
   const [markdown, setMarkdown] = useState("");
+  const [title, setTitle] = useState("");
   const [canvasItems, setCanvasItems] = useState([]);
   const [isCanvasExpanded, setIsCanvasExpanded] = useState(false);
+  const location = useLocation();
+  const titleVal = location.state?.title;
+  const markdownVal = location.state?.markdownVal;
+  useEffect(() => {
+      // console.log("Title: ", titleVal);
+      // console.log("Markdown: ", markdownVal);
+    if(titleVal){
+      setTitle(titleVal);
+    }
+    if(markdownVal){
+      setMarkdown(markdownVal); 
+    }
+  },[titleVal, markdownVal]);
+
+  useEffect(() => {
+    console.log("Markdown: ", markdown);
+    console.log("Title: ", title);
+
+  }, [markdown, title]);
 
   return (
     <div className={`${isCanvasExpanded ? "canvas-fullscreen" : "notes-editor-container"}`}>
       <div className="filler-item"  />
-      <MarkdownEditor />
+      <MarkdownEditor titleVal={title} markdownVal={markdown}/>
       <Canvas onToggleFullscreen={setIsCanvasExpanded} />
     </div>
   );
