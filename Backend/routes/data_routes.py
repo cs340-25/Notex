8ba05fc3c4125e11a17594dd153f2b4958b5babd
Colsen <166042861+ColsenUTK@users.noreply.github.com>
2025-05-05 15:36:57 -0400

    commented routing code

--- Backend/routes/data_routes.py
+++ Backend/routes/data_routes.py
@@ -8,23 +8,25 @@ from db_connection.delete import delete_canvas_data, delete_folder_data, delete_
 
 data_bp = Blueprint('data', __name__, url_prefix='/data')
 
-mock_data_store = {}
-
-
+# Get all current data for a user
 @data_bp.route('', methods=['GET'])
 def get_data():
+    # Get the username from the request
     username = request.args.get('username')
     if not username:
         return jsonify({"error": "Username is required"}), 400
     
+    # Connect to the database
     db_connection = Conn()
     conn = db_connection.conn
     cursor = conn.cursor()
     try:
+        # Get the user id from the username
         user_id = get_user_id(cursor, username)
         if not user_id:
             return jsonify({"error": "User not found"}), 404
         
+        # Get all the data for the user
         data = read_all_user_data(cursor, conn, user_id)
         if data:
             return jsonify(data), 200
@@ -32,24 +34,32 @@ def get_data():
             return jsonify({"error": "No data found"}), 404
     
     finally:
+        # Close the connection to the database
         db_connection.close(cursor)
 
+# Get a note by id
 @data_bp.route('/noteid', methods=['GET'])
 def get_note_data():
+    # Get the username from the request
     username = request.args.get('username')
     if not username:
         return jsonify({"error": "Username is required"}), 400
     
+    # Connect to the database
     db_connection = Conn()
     conn = db_connection.conn
     cursor = conn.cursor()
     try:
+        # Get the user id from the username
         user_id = get_user_id(cursor, username)
         if not user_id:
             return jsonify({"error": "User not found"}), 404
+        
+        # Get the folder id and title from the request
         folder_id = request.args.get('folder_id')
         title = request.args.get('title')
 
+        # Get the note id from the folder id and title
         data = get_note_id(cursor, user_id, folder_id, title)
         if data:
             return jsonify(data), 200
@@ -59,22 +69,29 @@ def get_note_data():
     finally:
         db_connection.close(cursor)
 
+# Get a folder by id
 @data_bp.route('/folderid', methods=['GET'])
 def get_folder_data():
+    # Get the username from the request
     username = request.args.get('username')
     if not username:
         return jsonify({"error": "Username is required"}), 400
     
+    # Connect to the database
     db_connection = Conn()
     conn = db_connection.conn
     cursor = conn.cursor()
     try:
+        # Get the user id from the username
         user_id = get_user_id(cursor, username)
         if not user_id:
             return jsonify({"error": "User not found"}), 404
+        
+        # Get the folder name and parent folder id from the request
         folder_name = request.args.get('folder_name')
         parent_folder_id = request.args.get('parent_folder_id')
 
+        # Get the folder id from the folder name and parent folder id
         data = get_folder_id(cursor, user_id, folder_name, parent_folder_id)
         if data:
             return jsonify(data), 200
@@ -84,26 +101,34 @@ def get_folder_data():
     finally:
         db_connection.close(cursor)
 
-
+# Create new data
 @data_bp.route('', methods=['POST'])
 def post_data():
+    # Get the username from the request
     username = request.args.get('username')
     json_data = request.get_json()
 
+    # Check if the username and data are provided
     if not username or not json_data:
         return jsonify({"error": "Username and data are required"}), 400
     
+    # Connect to the database
     db_connection = Conn()
     conn = db_connection.conn
     cursor = conn.cursor()
     
     try:
+        # Get the user id from the username
         user_id = get_user_id(cursor, username)
         if not user_id:
             return jsonify({"error": "User not found"}), 404
         
+        # Add the user id to the data
         json_data['user_id'] = user_id
+
         data_type = json_data.get('type')
+
+        # Match the data type to the appropriate function
         match data_type:
             case 'note':
                 success = insert_note_data(cursor, conn, json_data)
@@ -116,28 +141,38 @@ def post_data():
             case _:
                 success = False
         if success:
+            # Commit the changes to the database
             conn.commit()
             return jsonify({"message": "Data created", "data": json_data}), 201
         else:
             return jsonify({"error": "Insert failed"}), 500
     
     finally:
+        # Close the connection to the database
         db_connection.close(cursor)
 
+# Update data
 @data_bp.route('', methods=['PUT'])
 def put_data():
+    # Get the username from the request
     username = request.args.get('username')
     json_data = request.get_json()
 
+    # Connect to the database
     db_connection = Conn()
     conn = db_connection.conn
     cursor = conn.cursor()
     
+    # Check if the username and data are provided
     if not username or not json_data:
         return jsonify({"error": "Username and data are required"}), 400
+    
     try:
-        # user_id = get_user_id(cursor, username)
+        # Get the user id from the username
+        user_id = get_user_id(cursor, username)
         data_type = json_data.get('type')
+
+        # Match the data type to the appropriate function
         match data_type:
             case 'note':
                 success = update_note_data(cursor, conn, json_data)
@@ -150,30 +185,38 @@ def put_data():
             case _:
                 success = False
         if success:
+            # Commit the changes to the database
             conn.commit()
             return jsonify({"message": "Data Updated", "data": json_data}), 201
         else:
             return jsonify({"error": "Update failed"}), 500
         
     finally:
+        # Close the connection to the database
         db_connection.close(cursor)
 
-
+# Delete data
 @data_bp.route('', methods=['DELETE'])
 def delete_data():
+    # Get the username from the request
     username = request.args.get('username')
-    
+
+    # Connect to the database
     db_connection = Conn()
     conn = db_connection.conn
     cursor = conn.cursor()
 
+    # Check if the username is provided
     if not username:
         return jsonify({"error": "Username is required"}), 400
+    
     try:
         user_id = get_user_id(cursor, username)
-        
+
+        # Get the data type from the request
         data_type = request.args.get('type')
-        
+
+        # Match the data type to the appropriate function
         match data_type:
             case 'note':
                 json_data = {
@@ -196,10 +239,10 @@ def delete_data():
             case _:
                 success = False
         
-        #! This is a placeholder for actual deletion logic
         if success:
             return jsonify({"message": "Data deleted"}), 200
         else:
             return jsonify({"error": "Data not found"}), 404
     finally:
+        # Close the connection to the database
         db_connection.close(cursor)
--- Backend/routes/user_routes.py
+++ Backend/routes/user_routes.py
@@ -6,51 +6,65 @@ from db_connection.read import read_user_data
 from db_connection.delete import delete_user_data
 import bcrypt
 
+# User routes
 user_bp = Blueprint('user', __name__, url_prefix='/user')
 
-mock_user_store = {}
-
-
+# Get a user by username and password
 @user_bp.route('', methods=['GET'])
 def get_user():
+    # Get the username and password from the request
     username = request.args.get('username')
     password = request.args.get('password')
     if not username or not password:
         return jsonify({"error": "Username and password are required"}), 400
     
+    # Connect to the database
     db_connection = Conn()
     conn = db_connection.conn
     cursor = conn.cursor()
     try:
+        # Get the user id from the username
         user_id = get_user_id(cursor, username)
+
+        # Get the user data from the database
         user = read_user_data(cursor, conn, {'user_id': user_id})
+
+        # Check if the user exists and the password is correct
         if user and bcrypt.hashpw(password.encode('utf-8'), user["password_hash"].encode('utf-8')):
             return jsonify({"message": "User authenticated", "user": user}), 200
         else:
             return jsonify({"error": "User not found or password is incorrect"}), 404
     finally:
+        # Close the connection to the database
         db_connection.close(cursor)
-        
 
+# Create a new user
 @user_bp.route('', methods=['POST'])
 def post_user():
+    # Get the data from the request
     data = request.get_json()
 
+    # Check if the username and password are provided
     if not data or "username" not in data or "password" not in data:
         return jsonify({"error": "Username and password are required"}), 400
     
+    # Get the username from the data
     username = data['username']
 
+    # Connect to the database
     db_connection = Conn()
     conn = db_connection.conn
     cursor = conn.cursor()
-
+    
     try:
+        # Check if the username already exists
         if get_user_id(cursor, username):
             return jsonify({"error": "Username already exists"}), 409
         
+        # Insert the user data into the database
         success = insert_user_data(cursor, conn, data)
         if success:
+            # Get the user id from the username
             user_id = get_user_id(cursor, username)
             conn.commit()
             return jsonify({"message": "User created", "user_id": {"id": user_id, **data}}), 201
@@ -58,9 +72,10 @@ def post_user():
             return jsonify({"error": "Failed to create user"}), 500
         
     finally:
+        # Close the connection to the database
         db_connection.close(cursor)
 
-
+# Update a user
 # @user_bp.route('', methods=['PUT'])
 # def put_user():
 #     username = request.args.get('username')
@@ -84,23 +99,34 @@ def post_user():
 #     finally:
 #         db_connection.close(cursor)
 
+# Delete a user
 @user_bp.route('', methods=['DELETE'])
 def delete_user():
+    # Get the username and password from the request
     username = request.args.get('username')
     password = request.args.get('password')
 
+    # Check if the username and password are provided
     if not username or not password:
         return jsonify({"error": "Username and password are required"}), 400
+    
+    # Connect to the database
     db_connection = Conn()
     conn = db_connection.conn
     cursor = conn.cursor()
     try:
+        # Get the user id from the username
         user_id = get_user_id(cursor, username)
+
+        # Get the user data from the database
         user = read_user_data(cursor, conn, {'user_id': user_id})
+
+        # Check if the user exists and the password is correct
         if user and bcrypt.hashpw(password.encode('utf-8'), user["password_hash"].encode('utf-8')):
             delete_user_data(cursor, conn, {"user_id":user_id})
             return jsonify({"message": "User deleted"}), 200
         else:
             return jsonify({"error": "User not found or password is incorrect"}), 404
     finally:
+        # Close the connection to the database
         db_connection.close(cursor)
